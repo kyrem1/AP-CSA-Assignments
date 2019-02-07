7fa6f4fe5558e714f3f88df636dea184fce395e8
kyrem1 <goingpiano@gmail.com>
2019-02-06 20:51:49 -0600

    Completed Half of Caesar Cipher Project

--- Mod 14/14.03 Ceasar Shift Cipher/CaesarTester.java
+++ Mod 14/14.03 Ceasar Shift Cipher/CaesarTester.java
@@ -1,17 +1,21 @@
 
 /**
- * Write a description of class CaesarTester here.
+ * Client Class for the Encryption and Decryption Objects
+ *
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author James Harbour
+ * @version 2/6/19
  */
 import java.util.Scanner;
 public class CaesarTester
 {
     
-    public static void main() {
-        //Create Instance of Scanner
+    public static void main(String[] args) {
+        //Create Instances of Scanner
+        //First one has delimiter as whitespace, and second has new line as delimiter
         Scanner in = new Scanner(System.in);
+        Scanner inLine = new Scanner(System.in);
+        inLine.useDelimiter("\\n");
         
         //User input for shift
         System.out.print("Input a shift key value from 0–25: ");
@@ -22,7 +26,49 @@ public class CaesarTester
             System.out.print("Please input a shift key value from 0–25: ");
             s = in.nextInt();
         }
-        
+
+
+        //System.out.println(Encryption.cipherAlphabet(s));     //Debug
+
+        //Display the alphabet and cipher alphabet.
+        System.out.printf("%18s %26s\n", "ORIGINAL ALPHABET:", Encryption.ALPHABET);
+        System.out.printf("%18s %26s\n\n", "CIPHER ALPHABET:", Encryption.cipherAlphabet(s));
+
+        //Options Menu
+        System.out.println("Enter the number corresponding to what you would like to do");
+        System.out.println("\t1. Encryption");
+        System.out.println("\t2. Decryption");
+        System.out.println("\t3. Exit\n");
+
+        //Continuous User Input
+        boolean userFinished = false;
+        while(!userFinished) {
+            System.out.print("What would you like to do: ");
+            int choice = in.nextInt();
+            if(choice == 1) {                //Encryption Choice
+                System.out.print("Enter the plaintext you would like to encrypt: ");
+                String plaintext = inLine.next().replaceAll(" ", "").toUpperCase(); //User input but spaces are removed
+                System.out.println(Encryption.encrypt(plaintext));
+            } else if(choice == 2) {        //Decryption Choice
+                /*
+                 *
+                 *
+                 * Implement Decryption Choice
+                 *
+                 *
+                 * */
+
+            } else if(choice == 3) {        //Exit Choice
+                userFinished = true;
+            }
+        }
+
+
+
+
+
+
+
     }
 
     
--- Mod 14/14.03 Ceasar Shift Cipher/Encryption.java
+++ Mod 14/14.03 Ceasar Shift Cipher/Encryption.java
@@ -1,19 +1,94 @@
 
 /**
- * Write a description of class Encryption here.
+ * Class to implement Caesar Cipher Encryption based on a key: 0-25.
+ *
+ * E_n(x) = (x + n)          if 0 ≤ x + n < 26
+ * E_n(x) = (x + n) − 26     if x + n ≥ 26
+ *
  * 
  * @author James Harbour
- * @version 2/6/2019
+ * @version 2/6/19
  */
 public class Encryption
 {
-    private int shift; 
     public static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String cipher = "";
     
-    public Encryption(int _shift) {
-        shift = _shift;
+    public Encryption() {
     }
-    
-    
+
+    /**
+     *
+     * @param n The shift key
+     * @return newAlphabet
+     */
+    public static String cipherAlphabet(int n) {
+        //Get char[] from ALPHABET String
+        char[] originalChars = ALPHABET.toCharArray();
+
+        //Create int[] for the values of each letter in the original char array
+        int[] originalInts = new int[originalChars.length];
+
+        //Populate the originalInts array with int values 0-25, accounting for the ascii
+        //N.B. ASCII values for Capital Alphabet is 65-90 inclusive
+        for(int i = 0; i < originalChars.length; i++) {
+            originalInts[i] = (int)originalChars[i]-65;
+        }
+
+        //Create int[] for cipherText values
+        int[] newInts = new int[originalInts.length];
+
+        //Iterate over original ints and use Encryption formula, then account for ascii
+        for(int i = 0; i < originalInts.length; i++) {
+            int k = originalInts[i];
+            if(k + n >= 0 && k + n < 26) {
+                newInts[i] = (k + n) + 65;
+            } else if(k + n >= 26) {
+                newInts[i] = ((k + n) - 26) + 65;
+            } else {
+                System.out.println("Error with k: " + k + "\tand i: " + i);
+            }
+        }
+
+        String newAlphabet = "";
+
+        for(int k : newInts) {
+            newAlphabet += Character.toString((char)k);
+        }
+
+        cipher = newAlphabet;
+
+        return newAlphabet;
+    }
+
+    /**
+     *
+     * @param plain The plaintext to be shifted
+     * @return The encrypted text
+     */
+    public static String encrypt(String plain) {
+        char[] originalChars = ALPHABET.toCharArray();
+        char[] cipherChars = cipher.toCharArray();
+        char[] plainChars = plain.toCharArray();
+
+        String newText = "";
+
+        for (int i = 0; i < plainChars.length; i++) {
+            int index = 0;
+            Character pChar = plainChars[i];
+            boolean charFound = false;
+            while (!charFound) {
+                if (pChar.equals(originalChars[index])) {
+                    newText += Character.toString(cipherChars[index]);
+                    charFound = true;
+                } else {
+                    index++;
+                }
+            }
+        }
+
+        return newText;
+
+    }
     
 }
