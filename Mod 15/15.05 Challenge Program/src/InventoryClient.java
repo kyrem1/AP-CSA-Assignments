f88ec335b5fe5c03d136c4dc0fe1cd740c062ee6
kyrem1 <goingpiano@gmail.com>
2019-02-22 18:08:23 -0600

    Began 15.05 Challenge Program

--- Mod 15/15.05 Challenge Program/src/InventoryClient.java
+++ Mod 15/15.05 Challenge Program/src/InventoryClient.java
@@ -7,11 +7,19 @@ import java.util.ArrayList;
  */
 public class InventoryClient {
     public static void main(String[] args) {
-        // TODO Instantiate Objects
+        // Instantiate objects
+        GasVehicle gasVehicle = new GasVehicle("Honda Accord", 14666.0, 400);
+        ElectricVehicle electricVehicle = new ElectricVehicle("Tesla Model X", 84699.0);
+        Tool jigsaw = new Tool("Jigsaw", 230.0);
+        Tool hacksaw = new Tool("Hacksaw", 39.99);
+
 
         // Instantiate and Populate ArrayList
         ArrayList<Product> inventory = new ArrayList<Product>();
-            // TODO Populate inventory
+        inventory.add(gasVehicle);
+        inventory.add(electricVehicle);
+        inventory.add(jigsaw);
+        inventory.add(hacksaw);
 
 
 
